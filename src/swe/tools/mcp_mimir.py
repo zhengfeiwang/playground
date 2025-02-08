05dadcee7c931e7e35fb2926292fb742a22c00b8
Zhengfei Wang (from Dev Box) <zhengfeiwang@microsoft.com>
2025-02-08 14:40:40 +0800

    update MCP Mimir

--- src/swe/tools/mcp_mimir.py
+++ src/swe/tools/mcp_mimir.py
@@ -1,24 +1,16 @@
+from mcp.server.fastmcp import FastMCP
 
-import argparse
+from mimir import Mimir
 
-from swe.tools.mimir import Mimir
-
-
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--id", type=int, required=True)
-    return parser.parse_args()
+mcp = FastMCP("Get Azure DevOps work item")
 
 
-def main(args: argparse.Namespace):
+@mcp.tool()
+def get_work_item(work_item_id: int) -> str:
     mimir = Mimir()
-    work_item = mimir.get_work_item(work_item_id=args.id)
-    print(work_item.id)
-    print(work_item.title)
-    print(work_item.assigned_to)
-    print(work_item.description)
+    work_item = mimir.get_work_item(work_item_id)
+    return f"Work item {work_item.id} has title {work_item.title} and is assigned to {work_item.assigned_to}, description: {work_item.description}."
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+    mcp.run(transport="sse")
